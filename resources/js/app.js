0a4d5ac958d52fb5a9d879b24a9e0c15c1dd7f26
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-07-12 21:52:56 +0200

    [UPD] header's Home page style

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,13 @@ import './bootstrap';
 import '~resources/scss/app.scss';
 import.meta.glob(['../img/**']);
 import * as bootstrap from 'bootstrap';
+
+const linkList=document.querySelectorAll('.listLink');
+
+linkList.forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector('.headerActive')?.classList.remove('headerActive');
+        link.classList.add('headerActive')
+        console.log
+    })
+})
--- resources/views/shared/header.blade.php
+++ resources/views/shared/header.blade.php
@@ -1,7 +1,23 @@
 <header>
     <div class="container">
-        <div class="row">
-            
+        <div class="row align-items-center">
+            <div class="col-3 py-4">
+                <img src="{{ Vite::asset('resources/img/DC_Comics_logo.svg.png') }}" alt="DC logo" width="80px">
+            </div>
+            <div class="col-9 fw-bold text-end" >
+                <ul class="m-0">
+                    <li><a href="#" class="listLink">CHARACTERS</a></li>
+                    <li><a href="#" class="listLink">COMICS</a></li>
+                    <li><a href="#" class="listLink">MOVIES</a></li>
+                    <li><a href="#" class="listLink">TV</a></li>
+                    <li><a href="#" class="listLink">GAMES</a></li>
+                    <li><a href="#" class="listLink">COLLECTORS</a></li>
+                    <li><a href="#" class="listLink">VIDEOS</a></li>
+                    <li><a href="#" class="listLink">FANS</a></li>
+                    <li><a href="#" class="listLink">NEWS</a></li>
+                    <li><a href="#" class="listLink">SHOP</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 </header>
