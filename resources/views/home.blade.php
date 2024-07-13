1998777507a163324cad728670d207e71e812cd4
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-07-13 03:04:22 +0200

    [UPD] section 3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,49 @@
 @endsection
 
 @section('main')
-<div class="container">
-    <div class="row">
-        
+    <div>
+        <img src="https://static.dc.com/2024-07/2024_07_11_Marquee_3x1.jpg?w=1200" alt="DC logo" class="w-100">
     </div>
-</div>
+    <div class="container position-relative">
+        <div class="bg-main text-light fw-bold fs-4 px-4 py-1 cur-series">CURRENT SERIES</div>
+        <div class="row">
+            
+        </div>
+    </div>
+    <div class="bg-main py-5" id="section-3">
+        <div class="container text-light">
+            <div class="row">
+                <div class="col-auto">
+                    <i class="fa-solid fa-tablet-screen-button"></i>
+                </div>
+                <div class="col py-3 me-5">
+                    DIGITAL COMICS
+                </div>
+                <div class="col-auto">
+                    <i class="fa-solid fa-shirt"></i>
+                </div>
+                <div class="col py-3 me-5">
+                    DC MERCHANDISE
+                </div>
+                <div class="col-auto">
+                    <i class="fa-solid fa-user-pen"></i>
+                </div>
+                <div class="col py-3 me-5">
+                    SUBSCRIPTION
+                </div>
+                <div class="col-auto">
+                    <i class="fa-solid fa-location-dot"></i>
+                </div>
+                <div class="col-auto py-3 me-5">
+                    COMIC SHOP LOCATOR
+                </div>
+                <div class="col-auto">
+                    <i class="fa-solid fa-bolt"></i>
+                </div>
+                <div class="col-auto py-3 me-5">
+                    DC POWER VISA
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
